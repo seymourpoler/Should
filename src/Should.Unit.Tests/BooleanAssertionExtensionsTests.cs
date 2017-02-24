3d540ad8e2ffabf4212b2c80d5c23da0e5adbdc6
Luis Rodríguez <luis.rodriguez@saludonnet.com>
2017-02-24 16:09:11 +0100

    ShouldBeTrue

--- src/Should.Unit.Tests/BooleanAssertionExtensionsTests.cs
+++ src/Should.Unit.Tests/BooleanAssertionExtensionsTests.cs
@@ -25,9 +25,37 @@ namespace Should.Unit.Tests
             try
             {
                 true.ShouldBeFalse(message);
-                Assert.Fail("swhould be false with message");
+                Assert.Fail("should be false with message");
             }
             catch (FalseException ex)
+            {
+                ex.Message.ShouldEqual(message);
+            }
+        }
+
+	    [Test]
+	    public void ShouldBeTrue()
+	    {
+	        true.ShouldBeTrue();
+	    }
+
+        [Test]
+        [ExpectedException(typeof(TrueException))]
+        public void ThrowsTrueException()
+        {
+            false.ShouldBeTrue();
+        }
+
+        [Test]
+        public void TrueExceptionWithMessage()
+        {
+            var message = "message";
+            try
+            {
+                false.ShouldBeTrue(message);
+                Assert.Fail("should be true with message");
+            }
+            catch (TrueException ex)
             {
                 ex.Message.ShouldEqual(message);
             }
---
+++ src/should/BooleanAssertionExtensions/ShouldBeTrue.cs
@@ -0,0 +1,29 @@
+﻿using Should.Core.Assertions;
+
+namespace Should
+{
+    public static partial class BooleanAssertionExtensions
+    {
+        /// <summary>
+        /// Verifies that an expression is true.
+        /// </summary>
+        /// <param name="condition">The condition to be inspected</param>
+        /// <exception cref="TrueException">Thrown when the condition is false</exception>
+        public static void ShouldBeTrue(this bool condition)
+        {
+            Assert.True(condition);
+        }
+
+        /// <summary>
+        /// Verifies that an expression is true.
+        /// </summary>
+        /// <param name="condition">The condition to be inspected</param>
+        /// <param name="userMessage">The message to be shown when the condition is false</param>
+        /// <exception cref="TrueException">Thrown when the condition is false</exception>
+        public static void ShouldBeTrue(this bool condition,
+                                        string userMessage)
+        {
+            Assert.True(condition, userMessage);
+        }
+    }
+}
