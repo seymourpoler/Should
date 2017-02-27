d275e13a51987ca5f17c3d3d9e91bb7c63260e0b
Seymour Poler <seymourpoler@gmail.com>
2017-02-27 18:45:50 +0100

    adding ShouldNotThrows

---
+++ src/Should.Core/Assertions/Assert.ShouldNotThrows.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Should.Core.Assertions;
+using Should.Core.Exceptions;
+
+namespace Should.Core.Assertions
+{
+	public partial class Assert
+	{
+		public static void NotThrows<T>(Action action) where T : Exception
+		{
+			NotThrows(typeof(T), action);
+		}
+
+		public static void NotThrows(Type exceptionType, Action action){
+			if(exceptionType == null){
+				throw new ArgumentNullException ();
+			}
+			if(action == null){
+				throw new ArgumentNullException ();
+			}
+
+			var exception = ExceptionRecorder.Record (action);
+			if (exception != null)
+				throw new DoesNotThrowException(exception);
+		}
+	}
+}
+
