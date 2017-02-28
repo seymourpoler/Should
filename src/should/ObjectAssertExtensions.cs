87ae0391903aaa5921f4d3f4fdaf75cf97adccc1
Luis Rodríguez <luis.rodriguez@saludonnet.com>
2017-02-28 16:45:42 +0100

    extract class

--- src/should/ObjectAssertExtensions.cs
+++ src/should/ObjectAssertExtensions.cs
@@ -142,18 +142,6 @@ namespace Should
         /// <summary>
         /// Verifies that an object is exactly the given type (and not a derived type).
         /// </summary>
-        /// <typeparam name="T">The type the object should be</typeparam>
-        /// <param name="object">The object to be evaluated</param>
-        /// <returns>The object, casted to type T when successful</returns>
-        /// <exception cref="IsTypeException">Thrown when the object is not the given type</exception>
-        public static T ShouldBeType<T>(this object @object)
-        {
-            return Assert.IsType<T>(@object);
-        }
-
-        /// <summary>
-        /// Verifies that an object is exactly the given type (and not a derived type).
-        /// </summary>
         /// <param name="object">The object to be evaluated</param>
         /// <param name="expectedType">The type the object should be</param>
         /// <exception cref="IsTypeException">Thrown when the object is not the given type</exception>
---
+++ src/should/ObjectAssertExtensions/ShouldBeType.cs
@@ -0,0 +1,19 @@
+﻿using Should.Core.Assertions;
+
+namespace Should
+{
+    public static partial class ShouldBeType
+    {
+        /// <summary>
+        /// Verifies that an object is exactly the given type (and not a derived type).
+        /// </summary>
+        /// <typeparam name="T">The type the object should be</typeparam>
+        /// <param name="object">The object to be evaluated</param>
+        /// <returns>The object, casted to type T when successful</returns>
+        /// <exception cref="IsTypeException">Thrown when the object is not the given type</exception>
+        public static T ShouldBeType<T>(this object @object)
+        {
+            return Assert.IsType<T>(@object);
+        }
+    }
+}
