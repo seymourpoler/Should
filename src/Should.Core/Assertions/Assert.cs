7d55e69e4bd9ff2c35a44574359af1d3de0c0309
Luis Rodríguez <luis.rodriguez@saludonnet.com>
2017-03-01 16:35:14 +0100

    extract partial class

--- src/Should.Core/Assertions/Assert.cs
+++ src/Should.Core/Assertions/Assert.cs
@@ -520,30 +520,6 @@ namespace Should.Core.Assertions
                 throw new IsAssignableFromException(expectedType, @object, userMessage);
         }
 
-        /// <summary>        
-        /// Verifies that an object is not exactly the given type.
-        /// </summary>
-        /// <typeparam name="T">The type the object should not be</typeparam>
-        /// <param name="object">The object to be evaluated</param>
-        /// <exception cref="IsNotTypeException">Thrown when the object is the given type</exception>
-        public static void IsNotType<T>(object @object)
-        {
-            IsNotType(typeof(T), @object);
-        }
-
-        /// <summary>
-        /// Verifies that an object is not exactly the given type.
-        /// </summary>
-        /// <param name="expectedType">The type the object should not be</param>
-        /// <param name="object">The object to be evaluated</param>
-        /// <exception cref="IsNotTypeException">Thrown when the object is the given type</exception>
-        public static void IsNotType(Type expectedType,
-                                     object @object)
-        {
-            if (expectedType.Equals(@object.GetType()))
-                throw new IsNotTypeException(expectedType, @object);
-        }
-
         /// <summary>
         /// Verifies that an object is exactly the given type (and not a derived type).
         /// </summary>
