296ec4ac23ddf8068f4a59833695a36dca828e3e
Luis Rodríguez <luis.rodriguez@saludonnet.com>
2017-03-01 16:08:26 +0100

    partial class

---
+++ src/should/ObjectAssertExtensions/ShouldNotBeType.cs
@@ -0,0 +1,18 @@
+﻿using Should.Core.Assertions;
+
+namespace Should
+{
+    public static partial class ObjectAssertExtensions
+    {
+        /// <summary>
+        /// Verifies that an object is not exactly the given type.
+        /// </summary>
+        /// <typeparam name="T">The type the object should not be</typeparam>
+        /// <param name="object">The object to be evaluated</param>
+        /// <exception cref="IsTypeException">Thrown when the object is the given type</exception>
+        public static void ShouldNotBeType<T>(this object @object)
+        {
+            Assert.IsNotType<T>(@object);
+        }
+    }
+}
