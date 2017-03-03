1bdf9cbd1106f0109f8acf189e2eab5afe3562bf
Luis Rodríguez <luis.rodriguez@saludonnet.com>
2017-03-03 16:00:05 +0100

    change return type

--- src/should/ObjectAssertExtensions/ShouldBeType.cs
+++ src/should/ObjectAssertExtensions/ShouldBeType.cs
@@ -11,9 +11,9 @@ namespace Should
         /// <param name="object">The object to be evaluated</param>
         /// <returns>The object, casted to type T when successful</returns>
         /// <exception cref="IsTypeException">Thrown when the object is not the given type</exception>
-        public static T ShouldBeType<T>(this object @object)
+        public static void ShouldBeType<T>(this object @object)
         {
-            return Assert.IsType<T>(@object);
+            Assert.IsType<T>(@object);
         }
     }
 }
