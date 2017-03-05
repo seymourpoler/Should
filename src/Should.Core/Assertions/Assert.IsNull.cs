558f35cf5a371440694c48aa8884b54cde5148b5
Seymour Poler <seymourpoler@gmail.com>
2017-03-05 08:23:18 +0100

    extract is null class

--- src/Should.Core/Assertions/Assert.IsNull.cs
+++ src/Should.Core/Assertions/Assert.IsNull.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using Should.Core.Exceptions;
 
 namespace Should.Core.Assertions
 {
 	public partial class Assert
 	{
-		public static void IsNull(object @object)
+		/// <summary>
+		/// Verifies that an object reference is null.
+		/// </summary>
+		/// <param name="object">The object to be inspected</param>
+		/// <exception cref="NullException">Thrown when the object reference is not null</exception>
+		public static void Null(object @object)
 		{
-			
+			if (@object != null)
+				throw new NullException(@object);
 		}
 	}
 }
--- src/Should.Core/Assertions/Assert.cs
+++ src/Should.Core/Assertions/Assert.cs
@@ -620,28 +620,6 @@ namespace Should.Core.Assertions
         }
 
         /// <summary>
-        /// Verifies that an object reference is not null.
-        /// </summary>
-        /// <param name="object">The object to be validated</param>
-        /// <exception cref="NotNullException">Thrown when the object is not null</exception>
-        /*public static void NotNull(object @object)
-        {
-            if (@object == null)
-                throw new NotNullException();
-        }*/
-
-        /// <summary>
-        /// Verifies that an object reference is not null.
-        /// </summary>
-        /// <param name="object">The object to be validated</param>
-        /// <exception cref="NotNullException">Thrown when the object is not null</exception>
-        /*public static void NotNull(object @object, string message)
-        {
-            if (@object == null)
-                throw new NotNullException(message);
-        }*/
-
-        /// <summary>
         /// Verifies that two objects are not the same instance.
         /// </summary>
         /// <param name="expected">The expected object instance</param>
@@ -658,11 +636,11 @@ namespace Should.Core.Assertions
         /// </summary>
         /// <param name="object">The object to be inspected</param>
         /// <exception cref="NullException">Thrown when the object reference is not null</exception>
-        public static void Null(object @object)
+        /*public static void Null(object @object)
         {
             if (@object != null)
                 throw new NullException(@object);
-        }
+        }*/
 
         /// <summary>
         /// Verifies that two objects are the same instance.
