12292cc769126a09e8319bc5d273b097f5de46bc
Seymour Poler <seymourpoler@gmail.com>
2017-03-06 16:17:35 +0100

    extract class

---
+++ src/Should.Core/Assertions/Assert.False.cs
@@ -0,0 +1,21 @@
+﻿using Should.Core.Exceptions;
+
+
+namespace Should.Core.Assertions
+{
+	public partial class Assert
+	{
+		/// <summary>
+		/// Verifies that the condition is false.
+		/// </summary>
+		/// <param name="condition">The condition to be tested</param>
+		/// <param name="userMessage">The message to show when the condition is not false</param>
+		/// <exception cref="FalseException">Thrown if the condition is not false</exception>
+		public static void False(bool condition, string userMessage)
+		{
+			if (condition)
+				throw new FalseException(userMessage);
+		}
+	}
+}
+
--- src/Should.Core/Assertions/Assert.cs
+++ src/Should.Core/Assertions/Assert.cs
@@ -353,18 +353,6 @@ namespace Should.Core.Assertions
             False(condition, null);
         }
 
-        /// <summary>
-        /// Verifies that the condition is false.
-        /// </summary>
-        /// <param name="condition">The condition to be tested</param>
-        /// <param name="userMessage">The message to show when the condition is not false</param>
-        /// <exception cref="FalseException">Thrown if the condition is not false</exception>
-        public static void False(bool condition, string userMessage)
-        {
-            if (condition)
-                throw new FalseException(userMessage);
-        }
-
         static IEqualityComparer<T> GetEqualityComparer<T>()
         {
             return new AssertEqualityComparer<T>();
