3c1240f5fc35719780accb5c5214b75f9f2c0967
Luis Rodríguez <luis.rodriguez@saludonnet.com>
2017-03-07 16:09:28 +0100

    adding CollectionAssertionExtensionsTests

---
+++ src/Should.Unit.Tests/CollectionAssertionExtensionsTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace Should.Unit.Tests
+{
+    [TestFixture]
+    public class CollectionAssertionExtensionsTests
+    {
+        [Test]
+        public void ShouldBeEmpty()
+        {
+            var list = new int[] {};
+
+            list.ShouldBeEmpty();
+        }
+    }
+}
