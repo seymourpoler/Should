9b52cf89ec1d2122b62de7220bea8b350c5c4eb6
Luis Rodríguez <luis.rodriguez@saludonnet.com>
2017-03-13 13:12:36 +0100

    adding fluent unit tests

---
+++ src/Should.Fluent.Unit.Tests/ShouldNotBeNullTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Should.Fluent.Unit.Tests
+{
+    [TestFixture]
+    public class ShouldNotBeNullTests
+    {
+        [Test]
+        public void ShouldNotBeNull()
+        {
+            "pepe".Should().Not.Be.Null();
+        }
+    }
+}
